3753c7cd3b15d1b234f933c597bf1fd5fe787807
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-11-06 21:14:36 -0300

    Update server.js

--- Projeto-Crud/server.js
+++ Projeto-Crud/server.js
@@ -16,6 +16,15 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'public', 'index.html')); //arquivo estático
 });
 
+//importar o meu arquivo de rotas (modular)
+const apiRoutes = require('./routes/api');
+
+//informar que é um arquivo JSON
+app.use(express.json());
+
+//passar a rota que virá do front
+app.use('/api/users/', apiRoutes);
+
 //vincular o nosso servidor em uma porta
 app.listen(port, ()=>{
     console.log("servidor web funcionando - backend");
